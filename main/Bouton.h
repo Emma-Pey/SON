df704186566cf75b28c42469d4f559db628fc80a
Emma-Pey <129041155+Emma-Pey@users.noreply.github.com>
2026-02-13 10:34:04 +0100

    deux effets

--- main/Bouton.h
+++ main/Bouton.h
@@ -3,9 +3,17 @@
 
 //#include <Arduino.h>
 #include <Audio.h>
-// #include "Pitch.h"
-// #include "noise_gate.h"
+#include "Pitch.h"
+#include "effect_dynamics.h" //noise gate
 // #include "Reverb.h"
+
+enum EffectType {
+  EFFECT_PITCH, 
+  EFFECT_NOISE,
+   //EFFECT_VARISPEED,
+//   EFFECT_REVERB,
+//   EFFECT_BITCRUSHER,
+  EFFECT_COUNT};
 
 class Bouton {
 public:
@@ -15,19 +23,22 @@ public:
 
     char filename[20];
 
+    int currentEffect;
+
     bool state;
     bool lastState;
 
     unsigned long pressStartTime;
     bool longPressTriggered;
 
-    float gain;   // 0.0 → 1.0
+    float gain;   // 0.0 → 1.0 ?
 
     // ======== Audio Objects ========
     AudioPlaySdRaw playRaw;
 
-    //Pitch pitch;
-    //noise_gate noise;
+    Pitch pitch;
+    //noise_gate noise; // ne fonctionne pas
+    AudioEffectDynamics noise;
     //AudioEffectVarispeed varispeed;
     //Reverb reverb;
     //AudioEffectBitcrusher bitcrusher;
@@ -35,8 +46,8 @@ public:
     //AudioMixer4 mixerDryWet;
 
     // ======== Connexions ========
-    // AudioConnection *patchCord1;
-    // AudioConnection *patchCord2;
+    AudioConnection *patchCord1;
+    AudioConnection *patchCord2;
     // AudioConnection *patchCord3;
     // AudioConnection *patchCord4;
     // AudioConnection *patchCord5;
@@ -46,11 +57,17 @@ public:
     Bouton(int number, int buttonPin);
 
     void begin();
-    void update();
+    void update(); //pas utile?
     void play();
 
     void setGain(float g);
+
+    void nextEffect();
+    void setEffectAmount(float value);
+
     float getOutput();
+
+    const char* getEffectName();
 
 private:
     float outputLevel;
