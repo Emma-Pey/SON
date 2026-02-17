32ce3ebda07940b5b14e4a25cfa5e1a7fa98f5f3
Emma-Pey <129041155+Emma-Pey@users.noreply.github.com>
2026-02-17 16:05:38 +0100

    tous les effets ok + pbm potentiomètre réglé

--- main/Bouton.cpp
+++ main/Bouton.cpp
@@ -10,18 +10,18 @@ Bouton::Bouton(int number, int buttonPin)
     gain = 1.0;
 
     // Création dynamique des connexions
-    //patchCord1 = new AudioConnection(playRaw, pitch);
-    //patchCord2 = new AudioConnection(pitch, noise);
-    //patchCord3 = new AudioConnection(noise, bitcrusher);
-
-    //patchCord3 = new AudioConnection(noise, varispeed);
-    //patchCord4 = new AudioConnection(varispeed, reverb);
-    //patchCord5 = new AudioConnection(reverb, bitcrusher);
+    patchCord1 = new AudioConnection(playRaw, pitch);
+    patchCord2 = new AudioConnection(pitch, noise);
+    patchCord4 = new AudioConnection(noise, mixer1);
+    //patchCord5 = new AudioConnection(varispeed, mixer1); 
 
     patchCord7 = new AudioConnection(mixer1, 0, delay1, 0);
     patchCord8 = new AudioConnection(delay1, 0, filter1, 0);
     patchCord9 = new AudioConnection(filter1, 0, mixer1, 1);
     patchCord10 = new AudioConnection(mixer1, 0, reverb1, 0);
+    patchCord11 = new AudioConnection(reverb1, 0, mixer1, 0);
+
+    patchCord6 = new AudioConnection(mixer1, bitcrusher);
 
 
 }
@@ -30,33 +30,39 @@ void Bouton::begin() {
     pinMode(pin, INPUT);
     sprintf(filename, "BUTTON%d.RAW", num); //créer le nom du fichier
 
-    //initialisation pour le noise gate, il faut mettre les autres aussi ?
+    // Speed
+    playRaw.setPlaybackRate(1.0f); // 1.0 = normal, >1 = plus rapide, <1 = plus lent
+    playRaw.enableInterpolation(true);
+    playRaw.setPlaybackRate(1.0);
+
+    basePitch = 0.0;  
+    speedCompensation = 0.0; 
+
+    // Noise Gate
     noise.gate(
         -40.0f,   // threshold plus bas
         0.8f,    // attack 80 ms
         0.8f,     // release 800 ms
         12.0f     // hysteresis large
     );
-    //sans les trucs juste après, ça clique
     noise.compression(0.0f);      // désactive comp
     noise.limit(0.0f);            // désactive limiter
     noise.makeupGain(0.0f);       // pas d'auto gain
 
+    // Bitcrusher
     bitcrusher.sampleRate(44100);
     bitcrusher.bits(16);
 
-    // --- Réglages précis du feedback ---
+    // Reverb
     mixer1.gain(0, 1.0);  // Volume direct
-    mixer1.gain(1, 0.45); // Baisse de l'echo
-
-    // --- Réglage Réverb ---
-    reverb1.reverbTime(0.5); // Temps de réverbération court pour lisser le son
-
+    mixer1.gain(1, 0.0); // Baisse de l'echo
+    mixer1.gain(2, 0.0);  // reverb OFF au départ
+    reverb1.reverbTime(0.6); // Temps de réverbération court pour lisser le son
     filter1.frequency(1000); 
     filter1.resonance(1.5);
 }
 
-void Bouton::update() {//pas utile ?
+void Bouton::update() {//pas utile parce qu'on le fait dans le main? mieux vaut le faire ici ou pas ?
 
     state = digitalRead(pin);
 
@@ -77,66 +83,80 @@ void Bouton::update() {//pas utile ?
 
 void Bouton::play() {
   playRaw.stop(); // on l'arrête
-  playRaw.play(filename); // on le relance
+  playRaw.playRaw(filename,1); // on le relance
 }
-
 
 // Effets
 void Bouton::setGain(float g) {
     gain = constrain(g, 0.0, 1.0);
-    //mixerDryWet.gain(0, gain);
 }
-
 
 void Bouton::nextEffect() {
   currentEffect++;
+  potLocked = true;  // On relock le potentiomètre
   if (currentEffect >= EFFECT_COUNT) currentEffect = 0;
 }
 
 void Bouton::setEffectAmount(float value) {//value entre 0 et 1023
+
+    int currentValue = effectValues[currentEffect];
+
+    // Si le pot n’a pas encore rattrapé la valeur mémorisée
+    if (potLocked) {
+        if (abs(value - currentValue) < pickupThreshold) {
+            potLocked = false; // On déverrouille
+        } else {
+            return; // On ignore le mouvement
+        }
+    }
+    effectValues[currentEffect] = value;
     switch(currentEffect) {
         case EFFECT_PITCH:
-            pitch.setParamValue("shift (semitones)", value*24/1023-12); // -12 - 12
+            basePitch = value*24/1023-12; //-12 à 12
+            pitch.setParamValue("shift (semitones)", basePitch + speedCompensation); 
             break;
-
-        /// à ajouter après (modifier les calculs pour notre cas):
 
         case EFFECT_NOISE:
-            //noise.setParamValue("Threshold",value*120/1023-120.0); // exemple
             noise.gate(value*120/1023-120.0);
-            //Serial.println(value*120/1023-120.0);
             break;
 
-    //     case EFFECT_VARISPEED:
-    //       varispeed.playbackRate(value * 2.0f); // 0..2
-    //       break;
+        case EFFECT_VARISPEED: {
+            double speed = 0.2 + (value / 1023.0) * 2.8;
+            playRaw.setPlaybackRate(speed);
 
-         case EFFECT_REVERB:
-            // 1. Réglage du temps de l'écho (50ms à 500ms)
-            int delayTime = map(value, 0, 1023, 50, 500);
+            speedCompensation = -log(speed) / log(2.0) * 12.0;
+            pitch.setParamValue("shift (semitones)", speedCompensation + basePitch); //compensation en prenant en compte la valeur actuelle du pitch
+            //Serial.println(speedCompensation);
+            break;
+        }
+
+        case EFFECT_REVERB: {
+            // 1. Réglage du temps de l'écho (0ms à 500ms)
+            int delayTime = map(value, 0, 1023, 0, 500);
             delay1.delay(0, delayTime);
 
-            // 2. Réglage de la profondeur de la réverb (0.0 à 1.0)
-            // On utilise enfin revSize !
-            float revSize = (float)value / 1023.0;
-            reverb1.reverbTime(revSize); 
+            // 2. Réglage du niveau de la réverb (0.0 à 1.0)
+            float wet = (float)value / 1023.0;  
+            mixer1.gain(2, wet);   // niveau reverb progressif
+            mixer1.gain(1, wet*0.5);   // niveau delay progressif limité à 0.7
 
             break;
-
-        case EFFECT_BITCRUSHER:
+        }
+        
+        case EFFECT_BITCRUSHER: {
             int rate = map(value, 0, 1023, 44100, 3000);
             bitcrusher.sampleRate(rate);
             int bits = map(value, 0, 1023, 16, 8);
             bitcrusher.bits(bits);
             break;
+        }
         
-        
-    }
+    }    
 }
 
 const char* Bouton::getEffectName(){
     static const char* names[] = {
-      "PITCH", "NOISE", "BITCRUSHER"// ,"VARISPEED", "REVERB", 
+      "PITCH", "NOISE","VARISPEED", "REVERB", "BITCRUSHER"
     };
     return names[currentEffect];
 }
